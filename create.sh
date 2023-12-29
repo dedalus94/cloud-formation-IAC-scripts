065dd7f384895d28edaed57833d205c082b25f0e
dario de stefano <dario.destefano@gmail.com>
2023-12-29 21:31:50 +0000

    adds bash script to update, adds a template for a network with subnets in different availability zones, adds a new parameter json for the new configuration.

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@
 # Parameters: 
 #  stack-name : str 
 #  template-body : yaml file 
-#  parameters : json 
+#  parameters : json file
 aws cloudformation create-stack --stack-name $1 \
     --template-body file://$2   \
     --parameters file://$3  \
